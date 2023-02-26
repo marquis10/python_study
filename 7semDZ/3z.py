dfaea7ae004789a8849a87a26fef60672bbbf0d7
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-02-27 00:09:28 +0500

    7semDZ_last

--- 7semDZ/3z.py
+++ 7semDZ/3z.py
@@ -10,3 +10,28 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку str
 str(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+class Worker:
+    def __init__(self, position, name, surname, wage, bonus):
+        self.position = position
+        self.name = name
+        self.surname = surname
+        self._income = {'wage': wage, 'bonus': bonus}
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+    def __str__(self):
+        return f'{self.position}, {self.name}, {self.surname}, {self._income.get("wage")}, {self._income.get("bonus")}'
+
+
+Personal_1 = Position('Инженер', 'Александр', 'Кузьмин', 35000, 20000)
+print(f'Через str: {Personal_1}. Полный доход = {Personal_1.get_total_income()}')
+print(f'{Personal_1.position} - {Personal_1.get_full_name()} - Полный доход = {Personal_1.get_total_income()}')
+
+Personal_2 = Position('Программист', 'Василий', 'Васильев', 45000, 30000)
+print(f'Через str: {Personal_2}. Полный доход = {Personal_2.get_total_income()}')
+print(f'{Personal_2.position} - {Personal_2.get_full_name()} - Полный доход = {Personal_2.get_total_income()}')
