2feab87bdc19a73ee3aa8de5eb8bd09a05d3bb4f
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-04-12 06:26:38 +0500

    10semDZ

--- 9semDZ/Урок 2. Коды примеров/easy/descrs_1.py
+++ 9semDZ/Урок 2. Коды примеров/easy/descrs_1.py
@@ -2,33 +2,37 @@
 
 
 class Worker:
-    """Класс-работник"""
-    def __init__(self, name, surname, hours, rate):
-        self.name = name
-        self.surname = surname
-        self.hours = hours
-        self.rate = rate
+   """Класс-работник"""
+   def __init__(self, name, surname, hours, rate):
+       self.name = name
+       self.surname = surname
+       self.hours = hours
+       self.rate = rate
 
-    def total_profit(self):
-        """Расчет зарплаты"""
-        return self.hours * self.rate
+   def total_profit(self):
+       """Расчет зарплаты"""
+       return self.hours * self.rate
 
 
 OBJ = Worker('Иван', 'Иванов', 10, 100)
 print(OBJ.total_profit())
+# 1000
 
 OBJ.hours = 10
 OBJ.rate = 100
 print(OBJ.total_profit())
+# 1000
 
 # теперь попробуем присвоить какому-либо из атрибутов отрицательное значение
 
 OBJ = Worker('Иван', 'Иванов', -10, 100)
 print(OBJ.total_profit())
+# -1000
 
 OBJ.hours = 10
 OBJ.rate = -100
 print(OBJ.total_profit())
+# -1000
 
 # проблема налицо - значение атрибута при присвоении не проходит валидацию
 # следовательно скрипт может отрабатывать некорректно
